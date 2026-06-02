06df11fde76460b08c3ac584cc86c5c50480c504
bo <62258504+OathOfSilence@users.noreply.github.com>
2026-06-02 21:04:29 +0800

    Log gender and age auxiliary training metrics

--- loss/make_loss.py
+++ loss/make_loss.py
@@ -30,19 +30,39 @@ def make_loss(cfg, num_classes):    # modified by gu
         print("label smooth on, numclasses:", num_classes)
 
     def add_attribute_loss(loss, gender_score=None, age_score=None, gender=None, age=None):
+        loss_details = {
+            'gender_loss': None,
+            'age_loss': None,
+            'weighted_gender_loss': None,
+            'weighted_age_loss': None,
+        }
         if gender_score is not None and gender is not None and cfg.MODEL.GENDER_LOSS_WEIGHT > 0 and (gender >= 0).any():
-            loss = loss + cfg.MODEL.GENDER_LOSS_WEIGHT * F.cross_entropy(gender_score, gender, ignore_index=-1)
+            gender_loss = F.cross_entropy(gender_score, gender, ignore_index=-1)
+            weighted_gender_loss = cfg.MODEL.GENDER_LOSS_WEIGHT * gender_loss
+            loss = loss + weighted_gender_loss
+            loss_details['gender_loss'] = gender_loss
+            loss_details['weighted_gender_loss'] = weighted_gender_loss
         if age_score is not None and age is not None and cfg.MODEL.AGE_LOSS_WEIGHT > 0 and (age >= 0).any():
-            loss = loss + cfg.MODEL.AGE_LOSS_WEIGHT * F.cross_entropy(age_score, age, ignore_index=-1)
+            age_loss = F.cross_entropy(age_score, age, ignore_index=-1)
+            weighted_age_loss = cfg.MODEL.AGE_LOSS_WEIGHT * age_loss
+            loss = loss + weighted_age_loss
+            loss_details['age_loss'] = age_loss
+            loss_details['weighted_age_loss'] = weighted_age_loss
+        return loss, loss_details
+
+    def format_loss_return(loss, loss_details, return_details):
+        if return_details:
+            return loss, loss_details
         return loss
 
     if sampler == 'softmax':
-        def loss_func(score, feat, target, gender_score=None, age_score=None, gender=None, age=None):
+        def loss_func(score, feat, target, gender_score=None, age_score=None, gender=None, age=None, return_details=False):
             loss = F.cross_entropy(score, target)
-            return add_attribute_loss(loss, gender_score, age_score, gender, age)
+            loss, loss_details = add_attribute_loss(loss, gender_score, age_score, gender, age)
+            return format_loss_return(loss, loss_details, return_details)
 
     elif cfg.DATALOADER.SAMPLER == 'softmax_triplet':
-        def loss_func(score, feat, target, target_cam, i2tscore=None, gender_score=None, age_score=None, gender=None, age=None):
+        def loss_func(score, feat, target, target_cam, i2tscore=None, gender_score=None, age_score=None, gender=None, age=None, return_details=False):
             if cfg.MODEL.METRIC_LOSS_TYPE == 'triplet':
                 if cfg.MODEL.IF_LABELSMOOTH == 'on':
                     if isinstance(score, list):
@@ -62,9 +82,9 @@ def make_loss(cfg, num_classes):    # modified by gu
                     if i2tscore != None:
                         I2TLOSS = xent(i2tscore, target)
                         loss = cfg.MODEL.I2T_LOSS_WEIGHT * I2TLOSS + loss
-                    loss = add_attribute_loss(loss, gender_score, age_score, gender, age)
+                    loss, loss_details = add_attribute_loss(loss, gender_score, age_score, gender, age)
 
-                    return loss
+                    return format_loss_return(loss, loss_details, return_details)
                 else:
                     if isinstance(score, list):
                         ID_LOSS = [F.cross_entropy(scor, target) for scor in score[0:]]
@@ -83,9 +103,9 @@ def make_loss(cfg, num_classes):    # modified by gu
                     if i2tscore != None:
                         I2TLOSS = F.cross_entropy(i2tscore, target)
                         loss = cfg.MODEL.I2T_LOSS_WEIGHT * I2TLOSS + loss
-                    loss = add_attribute_loss(loss, gender_score, age_score, gender, age)
+                    loss, loss_details = add_attribute_loss(loss, gender_score, age_score, gender, age)
 
-                    return loss
+                    return format_loss_return(loss, loss_details, return_details)
             else:
                 print('expected METRIC_LOSS_TYPE should be triplet'
                       'but got {}'.format(cfg.MODEL.METRIC_LOSS_TYPE))
